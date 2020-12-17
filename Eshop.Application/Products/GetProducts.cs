0402548ea8a8ce78fe28d9e0ad9295c8a18f4f4c
Bartek <tester.bszelag@gmail.com>
2020-12-17 21:41:19 +0100

    Add project files.

---
+++ Eshop.Application/Products/GetProducts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Application.Products
+{
+    class GetProducts
+    {
+    }
+}
---
+++ Eshop.Domain/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Domain.Models
+{
+    class Product
+    {
+    }
+}
