65b320b373cfea84a8a6e0012ab3c9985dccc580
Bartek <tester.bszelag@gmail.com>
2020-12-18 23:50:42 +0100

    Adding the product to the DataBase from the web App

--- Eshop.Application/Products/CreateProduct.cs
+++ Eshop.Application/Products/CreateProduct.cs
@@ -3,6 +3,7 @@ using Eshop.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Eshop.Application.Products
 {
@@ -15,14 +16,16 @@ namespace Eshop.Application.Products
             _context = context;
         }
 
-        public void Do(int id, string Name, string Description)
+        public async Task Do(string Name, string Description, decimal Value)
         {
             _context.Products.Add(new Product
             {
-                Id = id,
                 Name = Name,
-                Description = Description
+                Description = Description,
+                Value = Value
             });
+
+            await _context.SaveChangesAsync();
         }
 
     }
--- Eshop.UI/Pages/Index.cshtml.cs
+++ Eshop.UI/Pages/Index.cshtml.cs
@@ -5,21 +5,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eshop.Application.Products;
+using Eshop.Database;
 
 namespace Eshop.UI.Pages
 {
+
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext _ctx;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext ctx)
         {
-            _logger = logger;
+            _ctx = ctx;
         }
 
+
+        [BindProperty]
+        public ProductViewModel Product { get; set; }
+
+        public class ProductViewModel 
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal Value { get; set; }
+        }
         public void OnGet()
         {
 
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await new CreateProduct(_ctx).Do(Product.Name, Product.Description, Product.Value);
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
