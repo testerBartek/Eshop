99bf19073fb006dcb47e5103bf991c1658e233a2
Bartek <tester.bszelag@gmail.com>
2020-12-18 01:18:18 +0100

    Migraded database

---
+++ Eshop.Application/Products/CreateProduct.cs
@@ -0,0 +1,29 @@
+﻿using Eshop.Database;
+using Eshop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Application.Products
+{
+    public class CreateProduct
+    {
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = Name,
+                Description = Description
+            });
+        }
+
+    }
+}
--- Eshop.Database/ApplicationDbContext.cs
+++ Eshop.Database/ApplicationDbContext.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Eshop.Domain.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eshop.Database
 {
-    class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
