ab75cddec2402ad39fde750ed00e6dc2f559dd91
Bartek <tester.bszelag@gmail.com>
2021-01-05 23:49:41 +0100

    Make the razor file compilation after refreshing the page. (.cshtml files)

--- Eshop.UI/Startup.cs
+++ Eshop.UI/Startup.cs
@@ -25,8 +25,11 @@ namespace Eshop.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //database connection
             IServiceCollection serviceCollections = services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["DefaultConnection"]));
+
+            // .cshtml refreshing
+            services.AddRazorPages().AddRazorRuntimeCompilation();
 
             services.AddRazorPages();
         }
