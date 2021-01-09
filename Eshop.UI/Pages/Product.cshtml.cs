aac8b48cc378607f38a7aa81f938c200267d3d6e
Bartek <tester.bszelag@gmail.com>
2021-01-09 01:49:20 +0100

    Add the possibilty to display the products.

---
+++ Eshop.UI/Pages/Product.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Eshop.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Eshop.Application.Products;
+
+namespace Eshop.UI.Pages
+{
+    public class ProductModel : PageModel
+    {
+
+        private ApplicationDbContext _ctx;
+
+        public ProductModel(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public GetProduct.ProductViewModel Product { get; set; }
+
+        public IActionResult OnGet(string name)
+        {
+            Product = new GetProduct(_ctx).Do(name.Replace("-", " "));
+            if (Product == null)
+                return RedirectToPage("Index");
+            else
+                return Page();
+        }
+    }
+}
