655895a3d99ea3b3b523f0a8c14f0cb3dce400d9
Bartek <tester.bszelag@gmail.com>
2021-01-10 00:48:34 +0100

    Add the View Components Default and Small

---
+++ Eshop.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Eshop.Application.Cart;
+using Eshop.Database;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eshop.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _ctx;
+
+        public CartViewComponent(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IViewComponentResult Invoke(string view = "Default")
+        {
+            return View(view, new GetCart(HttpContext.Session, _ctx).Do());
+        }
+    }
+}
